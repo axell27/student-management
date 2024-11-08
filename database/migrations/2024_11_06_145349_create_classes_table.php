a1ad300d797ebc601c7621c4085eeee138d9c805
axell27 <sirjhonny147@gmail.com>
2024-11-09 00:32:02 +0800

    Activity 1 - Install livewire, create models, migration and seeder

--- database/migrations/2024_11_06_145349_create_classes_table.php
+++ database/migrations/2024_11_06_145349_create_classes_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -13,10 +11,10 @@ return new class extends Migration
     {
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
